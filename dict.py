41c80377a4a0c516581ecf711001ef3cbc7cef51
mkhajiit <mkhajiit15@gmail.com>
2025-04-27 17:46:30 +0900

    fix: 주석 추가

--- dict.py
+++ dict.py
@@ -5,6 +5,7 @@ settings = {
   "browser": "chrome",
   "owner" : "JH",
 }
-
+# 안쪽 따옴표를 작은따옴표로 사용해서 오류를 피한다
+print(f"언어:{settings['language']}")
 for key, value in settings.items():
   print(f"{key} : {value}")
