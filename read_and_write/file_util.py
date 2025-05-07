41fe794c6357ba9b681c05b7a988c5d80963bd3b
mkhajiit <mkhajiit15@gmail.com>
2025-05-07 21:00:25 +0900

    feat: 메모장 읽어오기 기능 구현

--- read_and_write/file_util.py
+++ read_and_write/file_util.py
@@ -3,3 +3,9 @@ def save_to_file(todos, filename="todo_list.txt"):
     for task in todos:
       f.write(task+"\n")
 
+def load_from_file(filename="todo_list.txt"):
+  try:
+    with open(filename,"r",encoding="utf-8") as f:
+      return [line.strip() for line in f.readlines()]
+  except FileNotFoundError:
+    return []
