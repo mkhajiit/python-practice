5a1e9a227e5681b2fb5a575b1b6a774ace8be5bc
mkhajiit <mkhajiit15@gmail.com>
2025-05-15 21:30:05 +0900

    feat: dictionary 메서드 테스트:

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,19 @@
 grade = {"Yeon": 90, "Kim" : 70, "Yang": 65}
 
 print(grade["Yeon"])
+print(grade.get('Yeon'))
 
 obj = grade.keys()
+values_obj = grade.values()
+# key, value 쌍을 튜플로 묶은 값을 리턴
+items_obj = grade.items()
 
 # 객체에 저장된 데이터 하나씩 불러오기
-for i in obj:
-  print(i)
+for k in obj:
+  print("key:",k)
+
+for v in values_obj:
+  print("value:",v)
+
+for i in items_obj:
+  print("items:",i)
